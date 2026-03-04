e4034eba4e156b66c16c9713e6fb9880dd6990e3
JOÃO MARCOS <jmarcos.fbarros@gmail.com>
2026-03-04 13:22:14 -0300

    Update database connection details in comando.py

--- comando.py
+++ comando.py
@@ -6,22 +6,21 @@ load_dotenv()
 
 try:
     conexao = psycopg2.connect(
-        host=os.getenv('DB_HOST'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASS'),
-        database=os.getenv('DB_NAME'),
-        port=os.getenv('DB_PORT')
+        host="aws-0-sa-east-1.pooler.supabase.com",
+        user="postgres.ltaxntxapuekaguwuylj",
+        password=os.getenv('DB_PASS'), # Usando o Secret por segurança
+        database="postgres",
+        port="6543" # <-- A CHAVE ESTÁ AQUI
     )
     
     cursor = conexao.cursor()
-    print("✅ CONECTADO AO SUPABASE COM SUCESSO!")
+    print("✅ CONEXÃO ESTABELECIDA!")
 
     cursor.execute("SELECT * FROM vendas;")
     vendas = cursor.fetchall()
 
-    print("\n--- RELATÓRIO DE VENDAS ---")
     for venda in vendas:
-        print(f"ID: {venda[0]} | Produto: {venda[3]} | Valor: R$ {venda[2]}")
+        print(f"Produto: {venda[3]} | Valor: R$ {venda[2]}")
 
     cursor.close()
     conexao.close()
